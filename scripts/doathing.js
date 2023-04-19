ccc06658c0b770f199c11330dc5b3672f38e6fa4
KamiGug <32535551+KamiGug@users.noreply.github.com>
2023-04-20 01:39:01 +0200

    Update doathing.js

--- scripts/doathing.js
+++ scripts/doathing.js
@@ -8,10 +8,10 @@ function changeToLastMotive()
 
 function changeMotiveToDark()
 {
-    document.getElementById("changeStyle").href = "/styles/dark.css";
+    document.getElementById("changeStyle").href = "/black-red-trees/styles/dark.css";
 }
 
 function changeMotiveToLight()
 {
-    document.getElementById("changeStyle").href = "/styles/light.css";
+    document.getElementById("changeStyle").href = "/black-red-trees/styles/light.css";
 }
